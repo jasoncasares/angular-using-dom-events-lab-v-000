4b74abad6af9b8e5e7571e90bbccbaa59b226ac9
jasoncasares <jasonc@csdrafting.com>
2016-10-22 14:02:01 -0700

    Done.

--- js/app/directives/Counter.js
+++ js/app/directives/Counter.js
@@ -1,5 +1,6 @@
 function Counter() {
 	return {
+		transclue: true,
 		template: [
 			'<div>',
 				'<h3>Counter</h3>',
@@ -9,6 +10,16 @@ function Counter() {
 		].join(''),
 		controller: function ($scope) {
 			$scope.count = 0;
+		},
+		link: function (scope, element) {
+			element.on('click', function () {
+				scope.count += 1;
+
+				scope.$apply();
+			});
+			scope.$on('$destroy', function () {
+				element.off();
+			})
 		}
 	}
 }
